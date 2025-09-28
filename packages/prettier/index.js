f2f3c172bbaf307e418d604c752581c33ad1b23f
Fredrik Johansson <fredrik.johansson@thnx.se>
2025-09-28 17:32:28 +0200

    feat: replace Next.js ESLint config with React config and add Prettier/Tailwind packages

---
+++ packages/prettier/index.js
@@ -0,0 +1,38 @@
+import { fileURLToPath } from 'url'
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  singleQuote: true,
+  semi: false,
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  tailwindStylesheet: fileURLToPath(
+    new URL('../../tooling/tailwind/web.css', import.meta.url),
+  ),
+  tailwindFunctions: ['cn'],
+  importOrder: [
+    '<TYPES>',
+    '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
+    '^(next/(.*)$)|^(next$)',
+    '^(expo(.*)$)|^(expo$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '<TYPES>^@acme',
+    '^@acme/(.*)$',
+    '',
+    '<TYPES>^[.|..|~]',
+    '^~/',
+    '^[../]',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.7.3',
+}
+
+export default config
