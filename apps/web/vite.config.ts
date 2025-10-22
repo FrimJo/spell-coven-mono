2f2b2a3d9b4dc1f0510e9695faf3c590b6570c01
Fredrik Johansson <fredrik.johansson@thnx.se>
2025-10-22 02:29:54 +0200

    feat: add HTTPS support with vite-plugin-mkcert and configure dev server ports

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,18 +3,21 @@ import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => {
   return {
     // 🔴 important: include the trailing slash
     base: '/',
+
     plugins: [
       tanstackRouter({
         routesDirectory: './src/routes',
         generatedRouteTree: './src/routeTree.gen.ts',
       }),
       viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
+      mkcert({ savePath: './certificates' }),
       tailwindcss(),
       viteReact(),
     ],
@@ -25,7 +28,13 @@ export default defineConfig(() => {
       },
     },
     // (optional) if you import files from ../../packages during dev:
+    preview: {
+      port: 1234,
+      strictPort: true,
+    },
     server: {
+      port: 1234,
+      strictPort: true,
       fs: {
         allow: ['..'], // allow monorepo workspace imports
       },
